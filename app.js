8d3318c0243aef6621a1ef73c2ce5d508de1902a
Kaustabh04 <kaustabh04@gmail.com>
2016-10-19 23:56:13 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function(){
+	// Onclick of the button element
+    $("button").click(function(){
+		// Hide all input elements slowly
+        $("input").hide("slow");
+    });
+});
